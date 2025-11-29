d65740ccde0f6ea01e164a93affe5fffed72fd5e
oleksii-kryvobok <aleshakrivobok@gmail.com>
2025-11-29 16:47:59 +0200

    f commit changes# with '#' will be ignored, and an empty message aborts the commit. .git/COMMIT_EDITMSG [unix] (16:47 29/11/2025)# with '#' will be ignored, and an empty message aborts the commit. i tesss

---
+++ dist/index.js
@@ -0,0 +1,5 @@
+"use strict";
+let username = "John Doe";
+let age = 20;
+let tf = true;
+console.log(`${username}, ${age}; true/false?: ${tf}`);
---
+++ src/index.ts
@@ -0,0 +1,4 @@
+let username: string = "John Doe";
+let age: number = 20;
+let tf: boolean = true;
+console.log(`${username}, ${age}; true/false?: ${tf}`);
